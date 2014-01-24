5f726b9b5fc5149c0cf481845af885c2004b956c
Jiayu Zhou <dearjiayu@gmail.com>
2014-01-24 11:39:40 -0800

    test folders

---
+++ algorithms/recommendation/LMaFit.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 24, 2014
+
+@author: jiayu.zhou
+'''
+
+class LMaFit(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, initParam):
+        '''
+        Constructor
+        '''
+        
---
+++ data/daily_watchtime.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 24, 2014
+
+@author: jiayu.zhou
+'''        
+        
+
+def DailyWatchTimeReader(object):
+
+    fieldMapping = {'watchTime':3, 'genre':4};
+    
+    @staticmethod
+    def readLogFile(filename):
+        '''
+        This file reads a file
+        '''
+        
+    
