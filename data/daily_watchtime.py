a26b9f05165da896650b9f216e1eb40c138208fb
Jiayu Zhou <dearjiayu@gmail.com>
2014-01-24 15:26:29 -0800

    increment

--- data/daily_watchtime.py
+++ data/daily_watchtime.py
@@ -3,16 +3,55 @@ Created on Jan 24, 2014
 
 @author: jiayu.zhou
 '''        
-        
 
-def DailyWatchTimeReader(object):
+import csv;        
 
-    fieldMapping = {'watchTime':3, 'genre':4};
+class DailyWatchTimeReader(object):
+
+    def __init__(self):     
+        # the mapping from field to meanings. 
+        self.fieldMapping = {'duid':0, 'pid':1, 'watchtime':2, 'genre':3};
+        self.fieldDelimiter = '\t';
+        self.verbose = True;
     
-    @staticmethod
-    def readLogFile(filename):
+    def readLogFile(self, filename):
         '''
-        This file reads a file
+        This file reads a file, and return 
+        1. duid mapping (from duid to an integer, indicates the row number in the sparse matrix); 
+        2. pid mapping  (from pid  to an integer, indicates the column number in the sparse matrix);
+        3. core sparse matrix. 
+        4. a list for genre-program mapping. 
         '''
         
+        mapping_duid = {}; # store duid->row# mapping 
+        mapping_pid  = {}; # store pid->col# mapping
+        
+        row  = [];
+        col  = [];
+        data = [];
+        
+        with open(filename, 'rb') as csvfile:
+            logreader = csv.reader(csvfile, delimiter = self.fieldDelimiter, quotechar = '|');
+            for logrow in logreader:
+                log_duid      = logrow[self.fieldMapping['duid']];
+                log_pid       = logrow[self.fieldMapping['pid']];
+                log_watchtime = logrow[self.fieldMapping['watchtime']];
+                
+                if not (log_duid in mapping_duid):
+                    mapping_duid[log_duid] = len(mapping_duid);
+                row.append(mapping_duid[log_duid]);
+                
+                if not (log_pid in mapping_pid):
+                    mapping_pid[log_pid]   = len(mapping_pid);
+                col.append(mapping_pid[log_pid]);
+                
+                data.append(log_watchtime);
+        
+        if (self.verbose):
+            print 'Done reading agg log file. '+str(len(data)) + ' elements read'+ \
+                ' ( '+str(len(mapping_duid))+' row/user, '+str(len(mapping_pid))+' col/program).';
+        
+        return [mapping_duid, mapping_pid, row, col, data];
+                
     
+                
---
+++ data/test_daily_watchtime.py
@@ -0,0 +1,21 @@
+'''
+Created on Jan 24, 2014
+
+@author: jiayu.zhou
+'''
+
+import sys;
+from data.daily_watchtime import DailyWatchTimeReader
+
+if __name__ == '__main__':
+    filename = "../datasample/agg_duid_pid_watchtime_genre/20131209_100000";
+    
+    if len(sys.argv) == 0:
+        print 'Use default sample data.'
+    else:
+        filename = sys.argv[1];
+        
+    print 'processing file', filename;
+    
+    reader = DailyWatchTimeReader();
+    reader.readLogFile(filename);
