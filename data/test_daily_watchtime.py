e6b941f924d760042ef3c73ce958a53b8f97dd39
Jiayu Zhou <dearjiayu@gmail.com>
2014-01-24 16:07:30 -0800

    bug fix

--- data/test_daily_watchtime.py
+++ data/test_daily_watchtime.py
@@ -10,7 +10,7 @@ from data.daily_watchtime import DailyWatchTimeReader
 if __name__ == '__main__':
     filename = "../datasample/agg_duid_pid_watchtime_genre/20131209_100000";
     
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print 'Use default sample data.'
     else:
         filename = sys.argv[1];
