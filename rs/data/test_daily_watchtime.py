3a70f2e7b780e63c5143d96e4825da742beaab10
code-terminator <s.chang1@2030001331-scha.local>
2014-01-27 20:46:33 -0800

    My first commit

--- rs/data/test_daily_watchtime.py
+++ rs/data/test_daily_watchtime.py
@@ -11,8 +11,8 @@ from rs.visualization.histgram import histplot;
 from rs.data.daily_watchtime import DailyWatchTimeReader
 
 if __name__ == '__main__':
-    #filename = "../datasample/agg_duid_pid_watchtime_genre/20131209_100000";
-    filename = "/Users/jiayu.zhou/Data/duid-program-watchTime-genre/20131209/part-r-00000";
+    filename = "../../datasample/agg_duid_pid_watchtime_genre/20131209_100000";
+    #filename = "/Users/jiayu.zhou/Data/duid-program-watchTime-genre/20131209/part-r-00000";
     
     if len(sys.argv) == 1:
         print 'Use default sample data.'
