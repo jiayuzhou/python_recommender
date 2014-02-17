9d089c111ac0c0cf1a44db428981fdc930bf0619
Jiayu Zhou <dearjiayu@gmail.com>
2014-02-17 11:39:34 -0800

    Matlab variable bug.

--- runscripts/experimental/2014KDD/E01_DailyRecommend/exp_mean_avg_prec_time.py
+++ runscripts/experimental/2014KDD/E01_DailyRecommend/exp_mean_avg_prec_time.py
@@ -88,8 +88,8 @@ if __name__ == '__main__':
         
         #print '>>map:   ' 
         #print perf.tolist();
-            
-        matlab_output[method_name]= perf; 
+        
+        matlab_output[method_name.replace('.', '_')]= perf; 
         
     # save to file.
     hash_file_str = str(hash(tuple(daily_data_file))); 
